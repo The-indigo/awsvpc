497c9cbb57f9c135eadf8343e65d567e1b6b5c1c
The-indigo <adepojuadeyemi11@gmail.com>
2023-06-16 10:40:44 -0400

    fixes

--- web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install wget apache2 unzip -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-unzip -o 2117_infinite_loop.zip
-cp -r 2117_infinite_loop/* /var/www/html
-systemctl restart apache2
